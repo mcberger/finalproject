2c9b9f0968dba936b284af9bec39cb7b725257da
Mai Berger <m.c.berger@icloud.com>
2016-01-06 13:41:27 +0100

    post and comment functionality complete, although must be styled. fixes #9, fixes #10 and fixes #11

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,36 @@
 class CommentsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
 
   def new
+    @comment = Comment.new
+    @post = Post.find_by_id(params[:post_id])
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      flash[:notice] = "Your comment was created successfully."
+      redirect_to @post
+    else
+      flash[:alert] = "There was a problem saving your comment."
+      redirect_to new_comment_path
+    end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    if @comment.destroy
+      flash[:notice] = "Your post was deleted successfully."
+    else
+      flash[:alert] = "There was a problem deleting your post."
+    end
+    redirect_to post_path @post
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :post_id, :user_id)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,56 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if current_user != nil && @post.save
+      flash[:notice] = "Your post was created successfully."
+      redirect_to post_path @post
+    else
+      flash[:alert] = "There was a problem saving your post."
+      redirect_to new_post_path
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    if post.update_attributes post_params
+      flash[:notice] = "Your post was updated successfully."
+      redirect_to post_path post
+    else
+      flash[:alert] = "There was a problem updating your post."
+      redirect_to(:back)
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "The post was deleted successfully."
+      redirect_to posts_path
+    else
+      flash[:alert] = "There was a problem deleting your post."
+      redirect_to post_path @post
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body, :title, :user_id)
   end
 end
